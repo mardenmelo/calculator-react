0a311a0b5e45e5f093e323d8b255a0572f10edf3
Marden Melo <mardenmelo@gmail.com>
2021-01-22 12:45:49 -0300

    Completed Project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Projeto Calculadora React
+
+This project was bootstrapped with 
+[Create React App](https://github.com/facebook/create-react-app).
+
+Run
+npm start 
+http://localhost:3000
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Calculator from './main/Calculator';
+
+
+ReactDOM.render(
+  <div>
+    <h2>Calculadora</h2>    
+    <Calculator/>
+  </div>,
+  document.getElementById('root')
+);
